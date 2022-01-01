ea7b40f362adaa8db439ae811d3b7e55b13cd103
Sourav Biswas <96281246+Krishnavan0@users.noreply.github.com>
2022-01-01 14:05:22 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,20 @@
+// jAVA SE 11 - 11.0.10 -  
+// Import required header files
+import javax swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.awt.Font;
+import java.awt.TextArea;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Camparator;
+import java.util.TreeMap;
+
+
+public class{
+    public static void main (String args []){
+        System.out.println ("My first Java program : Hallo World");
+    }
+}
