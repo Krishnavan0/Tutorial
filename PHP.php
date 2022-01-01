c4f0f2872bb1d25889752f862bc26f4721e543b4
Sourav Biswas <96281246+Krishnavan0@users.noreply.github.com>
2022-01-01 16:34:12 +0530

    Add files via upload

---
+++ PHP.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Sourav Biswas">
+    <meta name="discription" content="This world most Searching Platform For Performing Demonic Rituals">
+    <meta name="keywords" content="html,CSS">
+    <link rel="stylesheet" href="">
+    <title>Rasmus</title>
+</head>
+<body>
+    <div class="container">
+        This is my first php website
+        <?php 
+            echo "hallo world and this is printed using php";
+        ?>
+    </div>
+</body>
+</head>
