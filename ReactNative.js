0b7dc2606e8f70b6fd08d735545878745738e9a6
Sourav Biswas <96281246+Krishnavan0@users.noreply.github.com>
2022-03-08 09:13:57 +0530

    Add files via upload

---
+++ ReactNative.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+
+export class ReactNative extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+export default ReactNative;
